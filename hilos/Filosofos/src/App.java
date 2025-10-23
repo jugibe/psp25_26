64b79b95dd7c651fc288ff7d1644f10af770e792
juanjo <juanjo@debian.juanjo.com>
2025-10-23 12:59:16 +0200

    23/10/2025*

--- hilos/Filosofos/src/App.java
+++ hilos/Filosofos/src/App.java
@@ -1,21 +1,26 @@
+/*
+ * psp 2025
+ * problema de los filosofos
+ * metodo principal
+ */
 public class App {
     public static void main(String[] args) throws Exception {
         //nombro a los filosofos
         String [] nombres= {"Aristotoels","Platon","Pitagoras","Kant", "Diogenes"};
 
-        
+        //Creo los tenedores uno por filosofo
         Tenedor[] tenedores = new Tenedor[nombres.length];
         for (int i = 0; i < nombres.length; i++) {
             tenedores[i] = new Tenedor();
         }
 
-        // Crear 5 filósofos, cada uno con su tenedor izquierdo y derecho
+        // Crear  filósofos, cada uno con su tenedor izquierdo y derecho
         Filosofo[] filosofos = new Filosofo[nombres.length];
         for (int i = 0; i < nombres.length; i++) {
             Tenedor izq = tenedores[i];
             Tenedor der = tenedores[(i + 1) % nombres.length]; // el último comparte con el primero
-            filosofos[i] = new Filosofo(nombres[i], izq, der);
-            filosofos[i].start();
+            filosofos[i] = new Filosofo(nombres[i], izq, der);//Creo el filosofo le paso el nombre y los tenedores que le tocan
+            filosofos[i].start(); //lo pongo a correr
         }
         
     }
